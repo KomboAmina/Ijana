b21dce502bbefba38b6a7fba7d729293d9d2c269
Amina Kombo <komboamina@protonmail.com>
2024-12-28 00:49:20 +0300

    Create About Route.

---
+++ src/about/base.php
@@ -0,0 +1,102 @@
+<section class="mb-1">
+
+    <div class="row justify-content-center g-4">
+
+        <div class="col-sm-12 col-md-4">
+
+            <div class="">
+
+                <h1>About Ijana</h1>
+
+                <p>v 0.1 Developed in December 2024</p>
+
+                <p><small>With PHPDesktop, Bootstrap 5, CSS3 and nothing to lose.</small></p>
+
+            </div>
+
+        </div>
+
+        <div class="col-sm-12 col-md-5">
+
+            <div class="">
+
+                <img src="<?php echo URL;?>public/img/about-1.jpg" alt="ijana logo" class="img-fluid rounded-4"/>
+
+            </div>
+
+        </div>
+
+        <div class="col-sm-12 col-md-3">
+
+            <div class="mb-0">
+
+                <div class="bg-dark text-light rounded-4">
+
+                    <div class="p-4">
+
+                        <p class="display-5">Ijana</p>
+                    
+                        <p>Means "hundred" in Kinyarwanda.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-sm-12 col-md-3">
+
+            <div class="">
+
+                <img src="<?php echo URL;?>public/img/about-2.jpg" alt="ijana logo" class="img-fluid rounded-4"/>
+
+            </div>
+
+        </div>
+
+        <div class="col-sm-12 col-md-9">
+
+            <div class="mb-4">
+
+                <div class="bg-dark text-light rounded-4">
+
+                    <div class="p-4">
+
+                        <div>The name is from a soundbite in an audio interview of a Rwandan engineer. I decided to look for its definition online and found it relevant.</div>
+
+                    </div>
+
+                </div>
+
+                <div class="row mt-4">
+
+                    <div class="col-sm-12 col-md-6">
+
+                        <div class="bg-warning rounded-4 p-4">
+
+                            <div><small>A Free Project By<br /><a href="https://www.aminakombo.work/about/?ref=ijana" target="_blank">Amina Kombo &rarr;</a></small></div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-sm-12 col-md-6">
+
+                    <h4>Thank you for reading this far.</h4>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        
+        
+
+    </div>
+
+</section>
